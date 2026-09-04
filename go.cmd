aa7ff9bc30dfd25b42dcf4a91a1ec41009e7198a
xcepeu <xcepeu@gmail.com>
2026-09-04 21:46:29 +0200

    added go.cmd for easier development

---
+++ go.cmd
@@ -0,0 +1,4 @@
+xcopy ".\lang\*.*" "D:\Foundry_PortableV14.366\Data\modules\quick-journal-page-sort\lang" /K /D /H /Y
+xcopy ".\scripts\*.*" "D:\Foundry_PortableV14.366\Data\modules\quick-journal-page-sort\scripts" /K /D /H /Y
+xcopy ".\styles\*.*" "D:\Foundry_PortableV14.366\Data\modules\quick-journal-page-sort\styles" /K /D /H /Y
+xcopy ".\*.json" "D:\Foundry_PortableV14.366\Data\modules\quick-journal-page-sort" /K /D /H /Y
